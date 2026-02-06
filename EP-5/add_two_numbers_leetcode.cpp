92a06c851e3bdb7a720a1d09089c7d2ede7c5d2a
Abhishek Mishra <abhishekmishra051028@gmail.com>
2026-02-06 10:36:58 +0530

    EP-5: Add Two Numbers in Linked List | LeetCode Solution

---
+++ EP-5/add_two_numbers_leetcode.cpp
@@ -0,0 +1,56 @@
+// LeetCode #2: Add Two Numbers
+// https://leetcode.com/problems/add-two-numbers/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* curr = dummyHead;
+        int carry = 0;
+        
+        while (l1 != NULL || l2 != NULL || carry != 0) {
+            int sum = carry;
+            
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            
+            if (l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+        
+        return dummyHead->next;
+    }
+};
+
+/*
+Time Complexity: O(max(m, n)) where m and n are lengths of l1 and l2
+Space Complexity: O(max(m, n)) for the result list
+
+Example:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+Explanation: 9999999 + 9999 = 10009998
+*/
